7b9b0a8817b8fefeec4294aeb650565b93aa8bf7
hrcn <hrcn1128@gmail.com>
2020-06-04 14:36:24 -0400

    combine multiple reducers, apply middleware

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const redux = require('redux');
+const reduxLogger = require('redux-logger')
+
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger()
 
 // type of the action
 const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 // action creater
 function buyCake() {
@@ -13,26 +19,60 @@ function buyCake() {
   }
 }
 
+function buyIceCream() {
+  return {
+    // define the action
+    type: BUY_ICECREAM
+  }
+}
+
 // reducer
 // (previousState, action) => newState
 const initialState = {
+  numOfCakes: 10,
+  numOfIceCreams: 20
+}
+
+const initialCakeState = {
   numOfCakes: 10
 }
 
-const reducer = (state = initialState, action) => {
+const initialIceCreamState = {
+  numOfIceCreams: 20
+}
+
+const cakeReducer = (state = initialCakeState, action) => {
   switch(action.type) {
     case BUY_CAKE: return {
       // make a copy of the state object and only update the numOfCakes
       ...state,
       numOfCakes: state.numOfCakes - 1
     }
+
+    default: return state
+  }
+}
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch(action.type) {
+    case BUY_ICECREAM: return {
+      // make a copy of the state object and only update the numOfCakes
+      ...state,
+      numOfIceCreams: state.numOfIceCreams - 1
+    }
  
     default: return state
   }
 }
 
+// combine reducers
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer
+})
+
 // store --- holds application state
-const store = createStore(reducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 // allow access to state via getState()
 console.log('initial state: ', store.getState());
@@ -42,5 +82,6 @@ const unsubscribe = store.subscribe(() => console.log('updated state: ', store.g
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 unsubscribe()
-
